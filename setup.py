365eed42236e0b1e954f446db4e51fc836b8d5c3
Andrew Ingram <andrew.ingram@titanemail.com>
2010-04-19 09:57:05 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='monster',
+      version='0.1',
+      description='Django backend for monster',
+      author='Andrew Ingram',
+      author_email='andy@andrewingram.net',
+      packages=find_packages(),
+      zip_safe=False,
+      )
